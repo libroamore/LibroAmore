fcd81a47c21725e7265beddca19d353aaee76a7c
ayzem <ashkipezi@gmail.com>
2026-09-01 16:45:08 -0500

    Update subtitles and descriptions in SERIES array

--- script.js
+++ script.js
@@ -1279,11 +1279,11 @@ const SERIES = [
   {
     id: "wittmore",
     title: "G.M. Fairy",
-    subtitle: "Libros de romace, Fantasía, Ciencia ficción",
+    subtitle: "Libros de romace, Fantasía y Ciencia ficción",
     image: "https://i.postimg.cc/25rZPQWB/1.png",
-    description: "На льду играют жестко. Влюбляются — еще жестче. Четыре самостоятельные истории из университета Уиттмор: капитан, вратарь, защитник и тафгай — и девушки, рядом с которыми правила команды перестают работать.",
+    description: "Esta es una serie ambientada en un mismo universo, situada en el pueblito paranormal más adorable de Oregón. Aquí hay un poquito de todo tipo de seres no humanos: desde cambiaformas hasta objetos conscientes, e incluso algunos encuentros con extraterrestres.",
     books: [
-      { number: "01", title: "Игра в любовь с форвардом", subtitle: "НАЧАЛО ИСТОРИИ", status: "✅ перевод завершен", id: "Say My Name" },
+      { number: "01", title: "The Totally Typical Tale of Mappy McMapface", subtitle: "Mappy x Miguel", status: "✅ Traducido", id: "The Totally Typical Tale of Mappy McMapface" },
       { number: "02", title: "Под защитой вратаря", subtitle: "ПРОДОЛЖЕНИЕ", status: "✅ перевод завершен", id: "MONSTROUS" },
       { number: "03", title: "Мой дерзкий защитник", subtitle: "ПРОДОЛЖЕНИЕ", status: "✅ перевод завершен", id: "Fervor" },
       { number: "3.1", title: "Мой дерзкий защитник. Бонус", subtitle: "БОНУС К КНИГЕ 3", status: "✅ перевод завершен", id: "mapa-constelado" },
